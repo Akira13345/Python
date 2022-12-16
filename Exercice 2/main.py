481f16c88f66d552cac2edb9952596aa497bb9a8
Tir Omar <77621684+Cursorr@users.noreply.github.com>
2022-12-16 14:56:29 +0100

    Un code plus propre

--- Exercice 2/main.py
+++ Exercice 2/main.py
@@ -1,11 +1,11 @@
 m = int(input("Saisir le numéro du mois : "))
 
-if m < 1 or m > 12:
+if not 12 >= m >= 0:
     print("Le numéro du mois doit être compris entre 1 et 12")
 else:
     if m == 2:
         print("Le mois numéro", m, "est février et il a 28 jours")
-    elif m == 4 or m == 6 or m == 9 or m == 11:
+    elif m in [4, 6, 9, 11]:
         print("Le mois numéro", m, "est un mois de 30 jours")
     else:
         print("Le mois numéro", m, "est un mois de 31 jours")
